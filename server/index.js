c37b1789cbac19cb219f438f42281f594c529f80
Ankit <Ankit>
2021-01-18 18:15:01 +0530

    Revert "[FEATURE] Backend API Structure"
    This reverts commit 9e1758ff49513ee401b538b04c62ecea138e1e97.

--- server/index.js
+++ server/index.js
@@ -4,18 +4,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import userRoutes from './routes/users';
-
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/api/users', userRoutes);
-
-const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT;
+const CONNECTION_URL = 'mongodb://localhost/cafeteria';
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
