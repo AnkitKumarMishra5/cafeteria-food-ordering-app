b718976d0592bedbfcdc0de85504ff4a23689661
Ankit <Ankit>
2021-01-23 21:15:53 +0530

    [FEATURE] Rendering in Success Page

--- client/src/components/Success/Success.js
+++ client/src/components/Success/Success.js
@@ -2,36 +2,37 @@ import React from 'react';
 import {Row, Col, Table } from 'react-bootstrap';
 import './Success.css';
 
-const Success = () => {
+const Success = ({user}) => {
   return (
     <>
     <Row className="justify-content-center" id="main-success-container">
     <Col md={4} xs={12} className="main-success-div">
+    <h2>Welcome {user.username} !</h2>
        <Table responsive="xl" className="new-table">
             <tbody>
             <tr>
                 <th>Registration ID: </th>
-                <td className="values"> 11707555</td>
+                <td className="values">{user.regId}</td>
               </tr>
               <tr>
-                <th>Name</th>
-                <td className="values"> Saaru Bhatia</td>
+                <th>Name: </th>
+                <td className="values">{user.username}</td>
               </tr>
               <tr>
-                <th>Organization</th>
-                <td className="values">Web Geeks</td>
+                <th>Organization: </th>
+                <td className="values">{user.orgName}</td>
               </tr>
               <tr>
-                <th>Employee ID</th>
-                <td className="values"> 11707969</td>
+                <th>Employee ID: </th>
+                <td className="values">{user.employeeId}</td>
               </tr>
               <tr>
-                <th>Email</th>
-                <td className="values email">saar@gmail.com</td>
+                <th>Email: </th>
+                <td className="values email">{user.email}</td>
               </tr>
               <tr>
-                <th>Mobile No.:</th>
-                <td className="values">8289019696</td>
+                <th>Mobile No.: </th>
+                <td className="values">{user.mobile}</td>
               </tr>
             </tbody>
           </Table>
